07daea1fe3721dc8d7734693457fbfa2597d3177
Nazar Yaremiiy <nazar.yaremiiy@gmail.com>
2025-04-01 16:35:37 +0300

    Add registration controller with token response and duplicate email check

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -43,60 +43,50 @@ export const loginUser = async (req, res) => {
   }
 };
 
-// export const loginUser = async (req, res) => {
-//   const { email, password } = req.body;
-
-//   try {
-//     const user = await User.findOne({ email });
-//     if (!user) return res.status(400).json({ message: 'User not found' });
-
-//     const isMatch = await bcrypt.compare(password, user.password);
-//     if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
-
-// const token = jwt.sign(
-//   { id: user._id, email: user.email },
-//   process.env.JWT_SECRET,
-//   { expiresIn: '2h' }
-// );
-
-//     res.status(200).json({
-//       message: 'Login successful',
-//       token,
-//       user: {
-//         id: user._id,
-//         username: user.username,
-//         email: user.email
-//       }
-//     });
-
-//   } catch (err) {
-//     res.status(500).json({ message: 'Login failed', error: err.message });
-//   }
-// };
-
 export const registerUser = async (req, res) => {
   const { username, email, password } = req.body;
 
   try {
-    const userExists = await User.findOne({ email });
-    if (userExists) {
-      return res.status(400).json({ message: 'User already exists' });
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({
+        message: 'Користувач вже існує!',
+      });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashPassword = await bcrypt.hash(password, 10);
 
     const newUser = new User({
       username,
       email,
-      password: hashedPassword,
+      password: hashPassword,
     });
 
     await newUser.save();
 
-    res.status(201).json({ message: 'User registered successfully' });
+    const token = jwt.sign(
+      {
+        id: newUser.id,
+        email: newUser.email,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: '30d' }
+    );
+
+    res.status(201).json({
+      message: 'Користувач успішно зареєстрований',
+      success: true,
+      token,
+      user: {
+        id: newUser.id,
+        username: newUser.username,
+        email: newUser.email,
+      },
+    });
   } catch (err) {
     res
       .status(500)
-      .json({ message: 'Registration failed', error: err.message });
+      .json({ message: 'Виникла помилка реєстрації!', error: err.message });
   }
 };
