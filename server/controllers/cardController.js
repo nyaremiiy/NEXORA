21b1345a854daa5b75e7e6614e981ac9c1ea52b9
Nazar Yaremiiy <nazar.yaremiiy@gmail.com>
2025-04-04 15:30:08 +0300

    Implement edit and delete functionality for cards on backend

--- server/controllers/cardController.js
+++ server/controllers/cardController.js
@@ -33,3 +33,43 @@ export async function getCards(req, res) {
     res.status(500).json({ message: 'Не вдалося отримати картки.' });
   }
 }
+
+export async function deleteCard(req, res) {
+  const { id } = req.params;
+
+  try {
+    const card = await Card.findOneAndDelete({
+      _id: id,
+      userId: req.user.id,
+    });
+
+    if (!card) {
+      return res.status(404).json({ message: 'Картка не знайдена' });
+    }
+
+    res.status(200).json({ message: 'Картку успішно видалено' });
+  } catch (error) {
+    console.error('Помилка видалення картки:', error);
+    res.status(500).json({ message: 'Не вдалося видалити картку' });
+  }
+}
+
+export async function updateCard(req, res) {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  try {
+    const card = await Card.findOneAndUpdate(
+      { _id: id, userId: req.user.id },
+      { title },
+      { new: true }
+    );
+
+    if (!card) return res.status(404).json({ message: 'Картка не знайдена' });
+
+    res.status(200).json(card);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Помилка оновлення картки' });
+  }
+}
--- server/routes/cards.js
+++ server/routes/cards.js
@@ -1,10 +1,17 @@
 import express from 'express';
-import { createCard, getCards } from '../controllers/cardController.js';
+import {
+  createCard,
+  getCards,
+  deleteCard,
+  updateCard,
+} from '../controllers/cardController.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = new express.Router();
 
 router.post('/create', verifyToken, createCard);
 router.get('/', verifyToken, getCards);
+router.delete('/:id', verifyToken, deleteCard);
+router.patch('/:id', verifyToken, updateCard);
 
 export default router;
